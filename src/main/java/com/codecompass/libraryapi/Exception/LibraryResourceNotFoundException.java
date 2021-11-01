70a75dd0758b121c435bd9c08d44a9519321589f
apple <mdshabbirhassan08@gmail.com>
2021-11-01 21:42:41 +0300

    Get Publisher API working

---
+++ src/main/java/com/codecompass/libraryapi/Exception/LibraryResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.codecompass.libraryapi.Exception;
+
+public class LibraryResourceNotFoundException extends Exception{
+    public LibraryResourceNotFoundException(String message) {
+        super(message);
+    }
+}
